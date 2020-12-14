99b977b2fbf3b92f5eafa562513fdf27159a75ba
wdiazplus <dev@wilmerdiaz.com>
2020-12-14 12:39:21 -0500

    feat(components): add Input component

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Calculator';
+
+const Input = () => {
+  return (
+    <div className="input">
+      <input type="text" readOnly />
+    </div>
+  );
+};
+
+export default Input;
