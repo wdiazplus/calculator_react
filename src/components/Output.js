2b54c47e5aa453b8ab7d5e21ea282b56c3cba953
wdiazplus <dev@wilmerdiaz.com>
2020-12-14 12:40:03 -0500

    feat(components): add Output component

---
+++ src/components/Output.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Calculator';
+
+const Output = () => {
+  return (
+    <div className="result">
+      <input type="text" readOnly />
+    </div>
+  );
+};
+
+export default Output;
