fb361d1b185b7741bcccfd71714b4ebd705629f2
wdiazplus <dev@wilmerdiaz.com>
2020-12-15 16:36:03 -0500

    feat(structure and functionality): basic Calculator running

---
+++ src/logic/reset.js
@@ -0,0 +1,10 @@
+// const click = (state,value)=>{
+//     if (value ==="AC"){
+//         console.log(state)
+//     }
+//     if(value ==="7" ){
+//         console.log(value)
+//     }
+// } 
+
+// export {click}
